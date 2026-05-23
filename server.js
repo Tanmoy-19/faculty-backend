2b6a012df3efbb83c37e217f16822f32e9d2723b
Tanmoy <layektanmoy11gmail@gmail.com>
2026-05-23 21:03:37 +0530

    Fixed CORS for deployed frontend

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 
 app.post("/submit-cv", (req, res) => {
